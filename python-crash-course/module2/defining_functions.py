1142412560f8921d053bf063d8f4bf40fbe7bb5f
Manuel Fontan <832315+manfontan@users.noreply.github.com>
2024-03-27 19:59:18 +0000

    Add module2 folder
    Signed-off-by: Manuel Fontan <832315+manfontan@users.noreply.github.com>

---
+++ python-crash-course/module2/defining_functions.py
@@ -0,0 +1,7 @@
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+
+greeting("Kay", "Software Engineering")
+greeting("Cameron", "Software Engineering")
