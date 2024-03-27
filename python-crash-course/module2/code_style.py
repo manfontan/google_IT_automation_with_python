cf67ea581b0ecde1d4f99833234301fe98b12908
Manuel Fontan <832315+manfontan@users.noreply.github.com>
2024-03-27 20:47:22 +0000

    Add principles of code reuse and code style practice code
    Signed-off-by: Manuel Fontan <832315+manfontan@users.noreply.github.com>

---
+++ python-crash-course/module2/code_style.py
@@ -0,0 +1,16 @@
+def calculate(d):
+    q = 3.14
+    z = q * (d ** 2)
+    print(z)
+
+calculate(5)
+
+# The code above is hard to read. We can improve the style to make it more readable.
+def circle_area(radius):
+    pi = 3.14
+    area = pi * (radius ** 2)
+    print(area)
+
+circle_area(5)
+
+# This is how you write a comment in Python
