cf55eec4218f765811f419cc859a4231c4f3fcf3
Manuel Fontan <832315+manfontan@users.noreply.github.com>
2024-09-15 11:55:26 +0100

    Add while loop examples
    Signed-off-by: Manuel Fontan <832315+manfontan@users.noreply.github.com>

--- python-crash-course/module3/more_while_loop_examples.py
+++ python-crash-course/module3/more_while_loop_examples.py
@@ -5,3 +5,15 @@ def attempts(n):
         x += 1
     print("Done")
 attempts(5)
+
+def get_username():
+    return input("Enter username: ")
+
+def valid_username(username):
+    return len(username) > 3
+
+username = get_username()
+
+while not valid_username(username):
+    print("Invalid username")
+    username = get_username()
